00184c698e00c0e4d9f0d071642aa09baee1fae5
thiswind <thiswind@gmail.com>
2021-12-25 09:25:25 +0000

    update

--- publish.sh
+++ publish.sh
@@ -3,7 +3,6 @@
 # Create QR codes for cases
 if [ "$(cat FREEZE_QRCODE)" == "True" ]; then
     echo "QR code already published."
-    exit 0
 else
     echo "Publishing QR code ..."
     python create_cases.py
@@ -11,14 +10,19 @@ else
 fi
 
 # Clone gh-pages branch into _site/
+echo "Clone gh-pages branch into _site/ ..."
 rm -rf _site
 mkdir -p _site/
 git clone -b gh-pages "$(git config remote.origin.url)" _site
+echo "Clone gh-pages branch into _site/ done."
 
 # Jekyll build, default output is _site/
+echo "Jekyll build, default output is _site/ ..."
 jekyll build
+echo "Jekyll build, default output is _site/ done."
 
 # Push _site to gh-pages
+echo "Push _site to gh-pages ..."
 wd="$(pwd)"
 cd _site || exit
 git config pull.rebase false
@@ -29,3 +33,6 @@ git commit -a -m "update"
 git status
 git push
 cd "$wd" || exit
+echo "Push _site to gh-pages done."
+
+echo "Publish done."

2c28d5e8c1b0a19c92a043886639d015bcb4ae55
thiswind <thiswind@gmail.com>
2021-12-25 09:17:33 +0000

    update

--- publish.sh
+++ publish.sh
@@ -9,17 +9,18 @@ else
     echo "QR code published."
 fi
 
-rm -rf _site &&
-    mkdir -p _site/ &&
-    git clone -b gh-pages "$(git config remote.origin.url)" _site &&
-    git config pull.rebase false
+rm -rf _site
+mkdir -p _site/
+git clone -b gh-pages "$(git config remote.origin.url)" _site
+git config pull.rebase false
 
 jekyll build
 
-cd _site &&
-    git config pull.rebase false &&
-    rm -f README.md &&
-    git add -A &&
-    git commit -a -m "update" &&
-    git push
-cd ..
+wd="$(pwd)"
+cd _site || exit
+git config pull.rebase false
+rm -f README.md
+git add -A
+git commit -a -m "update"
+git push
+cd "$wd" || exit
